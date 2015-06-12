7188297c209c176f717ee6500c4a90ff38f09642
JoshuaQuek <joven2468@gmail.com>
2015-06-12 16:53:22 +0800

    commiting up with more models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'This is some content for our awesome new blog'
+		@content_second = 'This is more content for more blog' 
+	end
+
+	def new
+
+	end
+	
+	def create
+
+	end
+
+	def edit 
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+end
